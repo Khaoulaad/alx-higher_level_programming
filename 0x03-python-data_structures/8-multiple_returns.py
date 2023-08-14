a7632921919b956bbb19fabdc93471e6edaedf49
Khaoulaad <ouajaa.khaoula@gmail.com>
2023-08-14 15:34:35 -0400

    0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	if len(sentence) == 0 :
-		new_tuple = (0, None)
-	else :
-		new_tuple = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        new_tuple = (0, None)
+    else:
+        new_tuple = (len(sentence), sentence[0])
 
-	return new_tuple
+    return new_tuple
