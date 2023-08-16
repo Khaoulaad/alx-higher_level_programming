24208347ccede1eaca3c13d72ea88c70714ab2ea
Khaoulaad <ouajaa.khaoula@gmail.com>
2023-08-16 19:28:57 -0400

    0

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    dict_keys = list(a_dictionary.keys())
+    dict_keys.sort()
+    for i in dict_keys:
+        print("{}: {}".format(i, a_dictionary.get(i)))
