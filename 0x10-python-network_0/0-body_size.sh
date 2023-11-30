67c03d9ae27597e17956620ec9e1c97f8120f129
Khaoulaad <ouajaa.khaoula.com>
2023-11-30 11:39:21 -0800

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-# script to get the body size of a request
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# Check if URL is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 URL"
+    exit 1
+fi
+
+# Use curl to get the body size in bytes
+size=$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}')
+
+# Display the size
+echo "$size"
 
