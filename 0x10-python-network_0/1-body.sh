9cd4d1b854a4be833eb554c64e5a5eaf34bb1833
Khaoulaad <ouajaa.khaoula.com>
2023-11-30 12:31:54 -0800

    0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -Ls "$1"
 
+url="$1"
+
+response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+if [ "$response" -eq 200 ]; then
+    curl -s "$url"
+fi
