35565d64c6b6a641d52900439fce3897ac2274be
Khaoulaad <ouajaa.khaoula.com>
2023-12-01 10:12:29 -0800

    0

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
+"""script for posting data to web servers
 """
-script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
-The letter must be sent in the variable q.
-If no argument is given, set q="".
-If the response body is properly JSON formatted and not empty, display the
-id and name like this: [<id>] <name>, Otherwise: Display Not a valid JSON
-if the JSON is invalid and Display No result if the JSON is empty
-"""
-import sys
-import requests
-
-
 if __name__ == "__main__":
+    import requests
+    import sys
     url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
-    payload = {"q": q}
-    response = requests.post(url, data=payload)
-    try:
-        json_outp = response.json()
-        if not json_outp:
-            print("No result")
-        else:
-            my_id = json_outp.get("id")
-            my_name = json_outp.get("name")
+    query = {'q': q}
+    response = requests.post(url, data=query)
+    if response.status_code != requests.codes.ok or len(response.text) <= 0:
+        print('No result')
+        sys.exit()
+    else:
+        try:
+            my_obj = response.json()
+            if len(my_obj) == 0:
+                print('No result')
+                sys.exit()
+            my_id = my_obj.get('id')
+            my_name = my_obj.get('name')
             print("[{}] {}".format(my_id, my_name))
-    except ValueError as invalid_json:
-        print("Not a valid JSON")
+        except ValueError as invalid_json:
+            print('Not a valid JSON')
