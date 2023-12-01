2b4d458625c231ca92c33f0ada40e5ae505d0ca3
Khaoulaad <ouajaa.khaoula.com>
2023-12-01 11:24:00 -0800

    0

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,6 +9,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    value = response.headers.get("X-Request-Id")
+    resp = requests.get(url)
+    value = resp.headers.get("X-Request-Id")
     print(value)
