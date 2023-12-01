2b615ee3b32346a957f8d9c000c749c195108382
Khaoulaad <ouajaa.khaoula.com>
2023-12-01 11:06:39 -0800

    0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the
-body of the response (decoded in utf-8).
-You have to manage urllib.error.HTTPError exceptions and
+Takes in a URL, sends a request to the URL 
 print: Error code: followed by the HTTP status code.
 """
 import urllib.request
@@ -13,7 +11,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as resp:
+        with urllib.request.urlopen(url) as response:
             responseText = resp.read().decode("utf-8")
             print(responseText)
     except urllib.error.HTTPError as er:
