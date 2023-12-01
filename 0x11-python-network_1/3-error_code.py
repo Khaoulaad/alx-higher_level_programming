e4ad6425736c921ccbc21f4badabf0118c728f50
Khaoulaad <ouajaa.khaoula.com>
2023-12-01 11:10:14 -0800

    0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,7 +11,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as resp:
             responseText = resp.read().decode("utf-8")
             print(responseText)
     except urllib.error.HTTPError as er:
