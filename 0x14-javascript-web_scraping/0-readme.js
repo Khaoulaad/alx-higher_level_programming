b36035a4fbccc668ed91d1156ca4a7ea3504e892
Khaoulaad <ouajaa.khaoula.com>
2024-01-03 08:22:54 -0800

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,18 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+<<<<<<< HEAD
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+=======
+fs.readFile(process.argv[2], 'utf8', function (err, dt) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(dt);
+>>>>>>> 039aea538b610d4216b0c72f444c17e9950922a5
+  }
 });
